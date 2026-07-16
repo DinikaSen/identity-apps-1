14cabf2a9c09eec0b05ae6eeb4369b4a338a0130
Madhavi Gayathri <madhavi.17@cse.mrt.ac.lk>
2026-07-16 10:23:50 +0530

    Render components based on correct permisions.

--- features/admin.users.v1/components/user-groups-edit.tsx
+++ features/admin.users.v1/components/user-groups-edit.tsx
@@ -16,6 +16,7 @@
  * under the License.
  */
 
+import { FeatureAccessConfigInterface, useRequiredScopes } from "@wso2is/access-control";
 import { updateResources } from "@wso2is/admin.core.v1/api/bulk-operations";
 import { AppState } from "@wso2is/admin.core.v1/store";
 import { userstoresConfig } from "@wso2is/admin.extensions.v1/configs/userstores";
@@ -89,7 +90,8 @@ export const UserGroupsList: FunctionComponent<UserGroupsPropsInterface> = (
     const groupsFeatureConfig: FeatureAccessConfigInterface = useSelector(
         (state: AppState) => state?.config?.ui?.features?.groups);
 
-    const hasGroupsUpdatePermission: boolean = useRequiredScopes(groupsFeatureConfig?.scopes?.update);
+    const groupUpdateScopes: string[] = groupsFeatureConfig?.scopes?.update ?? [];
+    const hasGroupsUpdatePermission: boolean = useRequiredScopes(groupUpdateScopes);
 
     /**
      * Group memberships are updated through the SCIM2 Groups PATCH API, which requires
